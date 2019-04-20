a6426a2bbe80b6fc31fbd13894561b49fab72597
ksugawara61 <katsuya@kokohadoko.net>
2019-04-20 20:38:35 +0900

    [add] Activity / Adapterの処理を追加

--- app/src/main/java/net/k2o_info/hatenaview/viewmodel/activity/MainActivity.kt
+++ app/src/main/java/net/k2o_info/hatenaview/viewmodel/activity/MainActivity.kt
@@ -1,13 +1,45 @@
 package net.k2o_info.hatenaview.viewmodel.activity
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import net.k2o_info.hatenaview.databinding.ActivityMainBinding
 import net.k2o_info.hatenaview.R
+import net.k2o_info.hatenaview.viewmodel.adapter.ArticleRecyclerAdapter
+import java.util.*
+import kotlin.collections.ArrayList
 
+/**
+ * メインアクティビティ
+ *
+ * @author katsuya
+ * @since 1.0.0
+ */
 class MainActivity : AppCompatActivity() {
 
+    /**
+     * アクティビティ生成時に呼ばれる
+     *
+     * @param savedInstanceState
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val recyclerView: RecyclerView = binding.recyclerView
+        val linearLayoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = linearLayoutManager
+
+        val recyclerAdapter = ArticleRecyclerAdapter(this)
+
+        // TODO: 暫定対処
+        val itemList = ArrayList<String>(Arrays.asList("1", "2", "3", "4", "5"))
+        recyclerAdapter.updateItems(itemList)
+
+        recyclerView.adapter = recyclerAdapter
     }
+
+
 }
---
+++ app/src/main/java/net/k2o_info/hatenaview/viewmodel/adapter/ArticleRecyclerAdapter.kt
@@ -0,0 +1,62 @@
+package net.k2o_info.hatenaview.viewmodel.adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import net.k2o_info.hatenaview.R
+
+class ArticleRecyclerAdapter(context: Context) : RecyclerView.Adapter<ViewHolder>() {
+
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+    private var list: List<String> = emptyList()
+
+    /**
+     * 新規ViewHolderが渡されたときに呼ばれる
+     *
+     * @param viewGroup ビューの要素
+     * @param viewType ビューの種類
+     * @return 新しいViewHolder
+     */
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(inflater.inflate(R.layout.adapter_article, viewGroup, false))
+    }
+
+    /**
+     * 要素が表示されたときに呼ばれる
+     *
+     * @param viewHolder ビューホルダー
+     * @param position 要素番号
+     */
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        val item = list.getOrNull(position)
+
+        if (item != null) {
+            // 変数の格納
+            //viewHolder.getBinding().setVariable(BR.photo, item)
+
+            // タップ時の処理
+            viewHolder.getView().setOnClickListener {
+                //listener.onClickedListener(it, item, position)
+            }
+        }
+    }
+
+    /**
+     * 要素数を返却
+     *
+     * @return 要素数
+     */
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    /**
+     * 要素のアップデート
+     */
+    fun updateItems(list: List<String>) {
+        this.list = list
+        notifyDataSetChanged()
+    }
+
+}
